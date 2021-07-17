56e46f28b2fe092bf5c71c17eef8d4fa3483b127
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-17 11:28:45 -0500

    Validando campos para un nuevo cliente

---
+++ js/nuevocliente.js
@@ -0,0 +1,32 @@
+(function() {
+    const formulario = document.querySelector('#formulario');
+
+    formulario.addEventListener('submit', validarCliente);
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+
+        if(validar(cliente)) {
+            // Mostrando mensaje
+            console.log('todos los campos obligatorios');
+            return;
+        }
+        console.log('validacion completa');
+    }
+
+    function validar(obj) {
+        return !Object.values(obj).every(input => input !== '');
+    }
+})();
